e8e188a759e1cadad0e65c82dbb24d896ae57de2
Cee <125638258+ceecedy@users.noreply.github.com>
2024-08-24 01:41:44 +0800

    Update streamlit_app.py
    incorporate pandas in the conditional statements.

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,6 +43,11 @@ if ingredients_list:
     for fruit_chosen in ingredients_list:
         # increment the values to the ingredient_strings.
         ingredients_string += fruit_chosen + ' '
+
+        # incorporate pandas here 
+        search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen, ' is ', search_on, '.')
+        
         # request from api
         fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_chosen)
         # display the response on api request with its subheader.
